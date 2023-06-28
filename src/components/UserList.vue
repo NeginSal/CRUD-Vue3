bf67104bbab655f48ace67017a59273ac4745e7d
NeginSal <neginsaljooghi@gmail.com>
2023-06-28 17:00:29 +0330

    edited:UserList & showing all users

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>this is userList component</div>
+  <div>
+    <div v-if="users">
+      <div v-for="user in users" :key="user.id">
+        <p>{{ user.name }}</p>
+        <p>{{ user.username }}</p>
+      </div>
+
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:8000/users')
+      .then(res => res.json())
+      .then(data => this.users = data)
+      .catch(err => console.log(err.message))
+  }
+}
 </script>
